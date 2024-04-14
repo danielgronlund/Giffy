02a2a3a960c2cfd3397559abdc5d431faaa3c309
Daniel Grönlund <just.danne@gmail.com>
2024-04-14 19:14:41 +0200

    Initial Commit

---
+++ Giffy/GiffyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GiffyApp.swift
+//  Giffy
+//
+//  Created by Daniel Grönlund on 2024-04-14.
+//
+
+import SwiftUI
+
+@main
+struct GiffyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
